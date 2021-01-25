4544ab337b45ae8306d07c482316bb47f2b432df
hamsternz <hamster@snap.net.nz>
2021-01-25 17:20:12 +1300

    Make petterns and bars sepeate objects

--- drummer.c
+++ drummer.c
@@ -14,23 +14,66 @@
 #define SAMPLE_RATE 44100
 #define BEATS_PER_BAR   4
 #define BAR_LEN        72
-#define LOOP_BARS       8
+#define LOOP_BARS      10
 
-const struct voice {
-    const int     pan;
-    const int     volume;
+static int bar = 0;
+static int tickOfBar = 0;
+static int sampleOfTick = 0;
+static int samplesPerBeat;
+static int samplesPerTick;
+static int samplesToGenerate;
+
+const struct Sounds {
     const int16_t *samples;
     const size_t  len;
-} voices[N_VOICES] = {
-   { 16,  128, drum_kick,  sizeof(drum_kick)/sizeof(int16_t)},
-   {  8,    0, drum_clap,  sizeof(drum_clap)/sizeof(int16_t)},
-   { 16,   20, drum_snare, sizeof(drum_snare)/sizeof(int16_t)},
-   { 18,   60, drum_hihat, sizeof(drum_hihat)/sizeof(int16_t)},
-   { 28,   30, drum_perc, sizeof(drum_perc)/sizeof(int16_t)},
-   {  4,   30, drum_perc, sizeof(drum_perc)/sizeof(int16_t)}
+} sounds[] = {
+   {drum_kick,  sizeof(drum_kick)/sizeof(int16_t)},
+   {drum_clap,  sizeof(drum_clap)/sizeof(int16_t)},
+   {drum_snare, sizeof(drum_snare)/sizeof(int16_t)},
+   {drum_hihat, sizeof(drum_hihat)/sizeof(int16_t)},
+   {drum_perc,  sizeof(drum_perc)/sizeof(int16_t)}
 };
 
-char patterns[N_VOICES][BAR_LEN] = {
+static struct voice {
+    int pan;
+    int volume;
+    int sample;
+    int pos;
+    int emph;
+} voices[N_VOICES] = {
+   { 16,  192, 0, 0, 0},
+   {  8,    0, 1, 0, 0},
+   { 16,   40, 2, 0, 0},
+   { 18,   80, 3, 0, 0},
+   { 28,   30, 4, 0, 0},
+   {  4,   30, 0, 0, 0}
+};
+
+struct Pattern {
+   char pattern[N_VOICES][BAR_LEN];
+};
+
+static struct Pattern pattern0 = {{
+//"012345678901234567890123456789012345678901234567890123456789012345678901"
+  "1                                                                       ",
+  "                                                                        ",
+  "1                                                                       ",
+  "                                                                        ",
+  "                                                                        ",
+  "                                                                        "
+}};
+
+static struct Pattern pattern1 = {{
+//"012345678901234567890123456789012345678901234567890123456789012345678901"
+  "9                                   1                                   ",
+  "                                                                        ",
+  "4        1        1        1        3        1        1        1        ",
+  "                                                                        ",
+  "                                                                        ",
+  "                                                                        "
+}};
+
+static struct Pattern pattern2 = {{
 //"012345678901234567890123456789012345678901234567890123456789012345678901"
   "9                                   1                                   ",
   "         1                 1                 1                 1        ",
@@ -38,8 +81,69 @@ char patterns[N_VOICES][BAR_LEN] = {
   "5        1        1        1        1        1                 1        ",
   "5                          1                 1                          ",
   "         1                          4                          1        "
+}};
+
+struct Pattern *patterns[LOOP_BARS] = {
+   &pattern0,
+   &pattern1,
+   &pattern2
 };
 
+int bars[LOOP_BARS] = { 1,1,1,2,2,2,2,2,1,0};
+
+
+
+void generate_sample(int16_t o[2]) {
+   int32_t samples[N_VOICES];
+   int32_t output[2];
+
+   // Start of new tick?
+   if(sampleOfTick == 0) {
+      for(int i = 0; i < N_VOICES; i++) {
+         if(patterns[bars[bar]]->pattern[i][tickOfBar] != ' ') {
+            voices[i].pos  = 1;
+            voices[i].emph = (patterns[bars[bar]]->pattern[i][tickOfBar]-'1') * 12;
+         }
+      }
+   }
+
+   for(int i = 0; i < N_VOICES; i++) {
+      samples[i] = 0;
+      if(voices[i].sample >= 0) {
+         samples[i] = sounds[voices[i].sample].samples[voices[i].pos ];
+         if(voices[i].pos != 0) {
+            voices[i].pos++;
+            if(voices[i].pos  >= sounds[voices[i].sample].len) {
+               voices[i].pos  = 0;
+            } 
+         }
+      }
+   }
+
+   output[0] = 0;
+   output[1] = 0;
+   for(int i = 0; i < N_VOICES; i++) {
+      int vol = voices[i].emph + voices[i].volume;
+      output[0] += vol * samples[i]*(voices[i].pan);
+      output[1] += vol * samples[i]*(32-voices[i].pan);
+   }
+
+   sampleOfTick++;
+   if(sampleOfTick == samplesPerTick) {
+      sampleOfTick = 0;
+      tickOfBar++;
+      if(tickOfBar == BAR_LEN) {
+         tickOfBar = 0;
+         bar++;
+      }
+   }
+
+   // Convert back to 16 bit
+   o[0] = output[0] / (32 *4 * 256);
+   o[1] = output[1] / (32 *4 * 256);
+}
+
+////////////////////////////////////////////////////////////////////////
 
 struct WaveFileHeader {
   uint8_t  chunkID[4];
@@ -59,8 +163,6 @@ struct WaveFileHeader {
   uint8_t  subchunk2Size[4];
 };
 
-int voicestate[N_VOICES];
-int voiceemph[N_VOICES];
 
 void intToU4(uint8_t *d, int val) {
    d[0] = val;
@@ -96,65 +198,21 @@ int output_header(FILE *f, int samples_to_generate) {
    }
    return 1;
 }
-
-static int bar = 0;
-static int tickOfBar = 0;
-static int sampleOfTick = 0;
-static int samplesPerBeat;
-static int samplesPerTick;
-static int samplesToGenerate;
-
-void generate_sample(FILE *f) {
-   int32_t samples[N_VOICES];
-   int32_t output[2];
-   int16_t o[2];
-
-   // Start of new tick?
-   if(sampleOfTick == 0) {
-      for(int i = 0; i < N_VOICES; i++) {
-         if(patterns[i][tickOfBar] != ' ') {
-            voicestate[i] = 1;
-            voiceemph[i] = (patterns[i][tickOfBar]-'1') * 12;
-         }
-      }
-   }
-
-   for(int i = 0; i < N_VOICES; i++) {
-      samples[i] = voices[i].samples[voicestate[i]];
-      if(voicestate[i] != 0) {
-         voicestate[i]++;
-         if(voicestate[i] >= voices[i].len) {
-            voicestate[i] = 0;
-         } 
-      }
-   }
-
-   output[0] = 0;
-   output[1] = 0;
-   for(int i = 0; i < N_VOICES; i++) {
-      int vol = voiceemph[i] + voices[i].volume;
-      output[0] += vol * samples[i]*(voices[i].pan);
-      output[1] += vol * samples[i]*(32-voices[i].pan);
-   }
-
-   sampleOfTick++;
-   if(sampleOfTick == samplesPerTick) {
-      sampleOfTick = 0;
-      tickOfBar++;
-      if(tickOfBar == BAR_LEN) {
-         tickOfBar = 0;
-         bar++;
-      }
-   }
-
-   // Convert back to 16 bit
-   o[0] = output[0] / (32 *4 * 256);
-   o[1] = output[1] / (32 *4 * 256);
-   fwrite(o, sizeof(o), 1, f);
+int write_sample(FILE *f, int16_t samples[2]) {
+   uint8_t o[4];
+   o[0] = samples[0];
+   o[1] = samples[0]>>8;
+   o[2] = samples[1];
+   o[3] = samples[1]>>8;
+   if(fwrite(o, sizeof(o), 1, f) != 1) 
+     return 0;
+   return 1;
 }
 
+////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[]) {
    FILE *f;
+   int16_t samples[2];
    samplesPerBeat = SAMPLE_RATE*60/BPM;
    samplesPerTick = samplesPerBeat * BEATS_PER_BAR / BAR_LEN;
    samplesToGenerate = samplesPerTick * BAR_LEN * LOOP_BARS;
@@ -170,7 +228,8 @@ int main(int argc, char *argv[]) {
    } 
    output_header(f, samplesToGenerate);
    for(int i = 0; i < samplesToGenerate; i++) {
-       generate_sample(f);
+       generate_sample(samples);
+       write_sample(f, samples);
    }
    fclose(f);
    printf("sampleOfTick %i\n",sampleOfTick);
